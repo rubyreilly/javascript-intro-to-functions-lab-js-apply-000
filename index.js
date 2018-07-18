cab0393a491850e7593e53f3812366ea47466001
rubyclaroreilly@gmail.com <rubyclaroreilly@gmail.com>
2018-07-18 01:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
+function logShout(string){
+  console.log(string.toUpperCase())
+  return string
+}
+  
+  
+function logWhisper(string){
+  console.log(string.toLowerCase())
+  return string
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase()===string){
+    return "I can't hear you!"
+  }
+  
+  if (string.toUpperCase()===string){
+    return "YES INDEED!"
+  }
+  
+  if(string==="I love you, Grandma."){
+    return "I love you, too."
+  }
+}
